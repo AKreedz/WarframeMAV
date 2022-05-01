c3c510c6668af2ddfb528ed245d170eb588ee0a7
AKreedz <43466895+AKreedz@users.noreply.github.com>
2022-05-01 16:05:37 +0800

    Add files via upload

---
+++ WarframeMAV.cs
@@ -0,0 +1,22 @@
+﻿
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+
+namespace WarframeMAV
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyStartUp
+    {
+        static HarmonyStartUp()
+        {
+
+            new Harmony("akreedz.rimworld.warframemav").PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("Warframe M-A-V Harmony Add");
+        }
+    }
+}
